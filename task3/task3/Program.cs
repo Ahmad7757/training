ce9b83eb154c00d9bb74f10cbc96da6e4c7e5fe4
AhmadDabbagh <ahmadbesherdabbagh@gmail.com>
2026-04-03 23:33:54 +0300

    first commit

---
+++ task3/task3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string ex = generate(" HELLO WORLD! ");
+            Console.WriteLine(ex);
+        Console.ReadKey();
+        }
+      
+        static string generate(string text)
+        {
+         
+
+
+            text = Regex.Replace(text, @"[^\p{L}\d\s-_]", "").Trim().ToLower();      
+           
+            text = Regex.Replace(text, @"[\s_-]+", "-");
+
+           
+            return text;
+        }
+        
+
+        static string Reverse(string text)
+        {
+            char[] array = text.ToCharArray();
+            Array.Reverse(array);
+            return new String(array);
+        }
+
+
+    }
+}
